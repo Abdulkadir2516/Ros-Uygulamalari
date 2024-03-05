721b9c21be11bde972950125dea6b97c6d05827a
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2024-03-05 18:32:43 +0300

    cember server tamamlandı

--- src/basit_uygulamalar/scripts/cember_server.py
+++ src/basit_uygulamalar/scripts/cember_server.py
@@ -2,4 +2,23 @@
 Uygulama 3: Çember Boyunca Hareket - Server Düğümü
 """
 
+import rospy
+from geometry_msgs.msg import Twist
+from basit_uygulamalar.msg import CemberHareket
 
+def cemberFonksiyonu(istek):
+    hiz_mesajı = Twist()
+    lineer_hiz = 0.5
+    hiz_mesajı.linear.x = 0.5
+    hiz_mesajı.angular.z = lineer_hiz / istek.yaricap
+    # W = V/r
+    # açısal hız = linear hız / yarı çap
+
+    while not rospy.is_shutdown:
+        pub.publish(hiz_mesajı)
+
+
+
+rospy.init_node("cember_hareket")
+pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+rospy.Service("cember_servis", CemberHareket, cemberFonksiyonu)

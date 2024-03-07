294048a4e1dcf54fa46a9a2face710d3a10a1cad
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2024-03-07 14:31:18 +0300

    cember şekilde dönüş bitti

--- src/basit_uygulamalar/scripts/cember_server.py
+++ src/basit_uygulamalar/scripts/cember_server.py
@@ -1,24 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 Uygulama 3: Çember Boyunca Hareket - Server Düğümü
 """
 
 import rospy
 from geometry_msgs.msg import Twist
-from basit_uygulamalar.msg import CemberHareket
+from basit_uygulamalar.srv import CemberHareket
 
 def cemberFonksiyonu(istek):
-    hiz_mesajı = Twist()
+    hiz_mesaji = Twist()
     lineer_hiz = 0.5
-    hiz_mesajı.linear.x = 0.5
-    hiz_mesajı.angular.z = lineer_hiz / istek.yaricap
-    # W = V/r
-    # açısal hız = linear hız / yarı çap
-
-    while not rospy.is_shutdown:
-        pub.publish(hiz_mesajı)
-
-
+    hiz_mesaji.linear.x = lineer_hiz
+    yaricap = istek.yaricap
+    # w = v / r
+    hiz_mesaji.angular.z = lineer_hiz / yaricap
+    while not rospy.is_shutdown():
+        pub.publish(hiz_mesaji)
 
 rospy.init_node("cember_hareket")
-pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
-rospy.Service("cember_servis", CemberHareket, cemberFonksiyonu)
+pub = rospy.Publisher("cmd_vel",Twist,queue_size=10)
+rospy.Service("cember_servis",CemberHareket,cemberFonksiyonu)
+rospy.spin()
